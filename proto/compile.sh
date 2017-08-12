c6beef99012be422b27028e77d41916e15995a9d
jergoo <jergoo@outlook.com>
2017-08-12 21:19:14 +0800

    update comment

--- proto/compile.sh
+++ proto/compile.sh
@@ -6,7 +6,7 @@ protoc -I . --go_out=plugins=grpc:. test/test.proto
 # 编译hello.proto
 protoc -I . --go_out=plugins=grpc:. hello/hello.proto
 
-# 编译google api
+# 编译google api，新版编译器可以省略M参数
 protoc -I . --go_out=plugins=grpc,Mgoogle/protobuf/descriptor.proto=github.com/golang/protobuf/protoc-gen-go/descriptor:. google/api/*.proto
 
 # 编译hello_http.proto
